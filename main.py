fbaa0758369f7cdbdfc34a8b7634425293ba7399
sobirovumurbek459-max <sobirovumurbek459@gmail.com>
2026-01-22 18:08:50 +0500

    Update Telegram bot token and greeting message

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = os.environ.get("8312097068:AAEiovVoDy1trH87yI-hDjqUt67khvlSpww")
+TOKEN = "8312097068:AAFHC0j59aA1bncScbmyYdMs6_evjoweOzA"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salom! 👋 Men Render’da ishlayapman 🚀")
+    await update.message.reply_text("Salom! Bot ishlayapti ✅")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
     app.run_polling()
 
 if __name__ == "__main__":
